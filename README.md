80c993fd47dce33ffb9ef8d15efefc2e76ebac4b
Kevin Malakoff <kmalakoff@gmail.com>
2020-04-19 21:58:53 -0700

    Add compatibility tests

--- README.md
+++ README.md
@@ -7,8 +7,8 @@ var joinDeep = require('join-deep');
 var assert = require('assert');
 
 var array1 = [1, [2, [3, [4]], 5]];
-assert.deepStrictEqual(joinDeep(array1, ', '), '1, 2, 3, 4, 5');
+assert.deepEqual(joinDeep(array1, ', '), '1, 2, 3, 4, 5');
 
 var array2 = [[], [[]], [[], [[[]]]]];
-assert.deepStrictEqual(joinDeep(array2, ', '), '');
+assert.deepEqual(joinDeep(array2, ', '), '');
 ```
--- compatibility/spec/joinDeep.test.js
+++ compatibility/spec/joinDeep.test.js
@@ -1,5 +1,6 @@
 var assert = require('assert');
-var joinDeep = require('..');
+
+var joinDeep = require('../..');
 
 describe('join methods', function () {
   it('should treat sparse arrays as dense', function () {
@@ -7,14 +8,14 @@ describe('join methods', function () {
     var expected = [1, 2, 3];
     expected.push(undefined, undefined, undefined);
 
-    assert.deepStrictEqual(joinDeep(array, ', '), expected.join(', '));
+    assert.deepEqual(joinDeep(array, ', '), expected.join(', '));
   });
 
   it('should work with extremely large arrays', function () {
     var expected = Array(5e5).join(', ');
 
     try {
-      assert.deepStrictEqual(joinDeep([expected]), expected);
+      assert.deepEqual(joinDeep([expected]), expected);
     } catch (e) {
       assert.ok(false, e.message);
     }
@@ -23,18 +24,18 @@ describe('join methods', function () {
   it('should work with empty arrays', function () {
     var array = [[], [[]], [[], [[[]]]]];
 
-    assert.deepStrictEqual(joinDeep(array, ', '), '');
+    assert.deepEqual(joinDeep(array, ', '), '');
   });
 
   it('should support flattening of nested arrays', function () {
     var array = [1, [2, [3, [4]], 5]];
 
-    assert.deepStrictEqual(joinDeep(array, ', '), '1, 2, 3, 4, 5');
+    assert.deepEqual(joinDeep(array, ', '), '1, 2, 3, 4, 5');
   });
 
   it('should return an empty array for non array-like objects', function () {
     var nonArray = { '0': 'a' };
 
-    assert.deepStrictEqual(joinDeep(nonArray, ', '), '');
+    assert.deepEqual(joinDeep(nonArray, ', '), '');
   });
 });
