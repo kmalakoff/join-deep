50901355a3426fb24789d6e8e13c5e7c3fee049f
Kevin Malakoff <kmalakoff@gmail.com>
2025-01-03 21:46:16 -0800

    Upgrade modules

--- test/spec/joinDeep.test.js
+++ test/spec/joinDeep.test.js
@@ -1,18 +1,18 @@
-var assert = require('assert');
+const assert = require('assert');
 
-var joinDeep = require('../..');
+const joinDeep = require('join-deep');
 
-describe('join methods', function () {
-  it('should treat sparse arrays as dense', function () {
-    var array = [[1, 2, 3], Array(3)];
-    var expected = [1, 2, 3];
+describe('join methods', () => {
+  it('should treat sparse arrays as dense', () => {
+    const array = [[1, 2, 3], Array(3)];
+    const expected = [1, 2, 3];
     expected.push(undefined, undefined, undefined);
 
     assert.deepEqual(joinDeep(array, ', '), expected.join(', '));
   });
 
-  it('should work with extremely large arrays', function () {
-    var expected = Array(5e5).join(', ');
+  it('should work with extremely large arrays', () => {
+    const expected = Array(5e5).join(', ');
 
     try {
       assert.deepEqual(joinDeep([expected]), expected);
@@ -21,20 +21,20 @@ describe('join methods', function () {
     }
   });
 
-  it('should work with empty arrays', function () {
-    var array = [[], [[]], [[], [[[]]]]];
+  it('should work with empty arrays', () => {
+    const array = [[], [[]], [[], [[[]]]]];
 
     assert.deepEqual(joinDeep(array, ', '), '');
   });
 
-  it('should support flattening of nested arrays', function () {
-    var array = [1, [2, [3, [4]], 5]];
+  it('should support flattening of nested arrays', () => {
+    const array = [1, [2, [3, [4]], 5]];
 
     assert.deepEqual(joinDeep(array, ', '), '1, 2, 3, 4, 5');
   });
 
-  it('should return an empty array for non array-like objects', function () {
-    var nonArray = { 0: 'a' };
+  it('should return an empty array for non array-like objects', () => {
+    const nonArray = { 0: 'a' };
 
     assert.deepEqual(joinDeep(nonArray, ', '), '');
   });
