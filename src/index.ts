5d9caff6f44b6beed60b468a5d24e965894d831d
Kevin Malakoff <kmalakoff@gmail.com>
2025-05-31 11:20:53 -0700

    Comprehensive types

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,18 @@
 import reduceDeep from 'reduce-deep';
 
-export default function joinDeep(array, sep) {
-  const length = array == null ? 0 : array.length;
+export type NestedArray<T> = T | NestedArray<T>[];
+
+export default function joinDeep<T>(array: NestedArray<T>[], sep: string) {
   let hasJoined = false;
 
-  return length
-    ? reduceDeep(
-        array,
-        (memo, value) => {
-          if (hasJoined) memo += sep;
-          else hasJoined = true;
-          if (value !== undefined) memo += value;
-          return memo;
-        },
-        ''
-      )
-    : '';
+  return reduceDeep(
+    array,
+    (memo, value) => {
+      if (hasJoined) memo += sep;
+      else hasJoined = true;
+      memo += value === undefined ? '' : value;
+      return memo;
+    },
+    ''
+  );
 }
